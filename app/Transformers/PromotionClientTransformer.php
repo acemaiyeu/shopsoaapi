bef10af717d3fda2e5b171bd7e6ecc769f160986
Huy FE <nthanhhuy82@gmail.com>
2025-03-02 21:43:56 +0700

    change show start_time and end_time promotion client

--- app/Transformers/PromotionClientTransformer.php
+++ app/Transformers/PromotionClientTransformer.php
@@ -6,11 +6,12 @@ use League\Fractal\TransformerAbstract;
 use App\Models\Promotion;
 use Akaunting\Money\Money;
 use App\Models\Product;
+use Carbon\Carbon;
 
 class PromotionClientTransformer extends TransformerAbstract
 {
     protected $prodcts = [];
-    public function __construct($products)
+    public function __construct($products = null)
     {
         $this->products = $products;
     }
@@ -21,8 +22,8 @@ class PromotionClientTransformer extends TransformerAbstract
                 'id'            => $promotion->id,
                 'code'          => $promotion->promotion_code,
                 'name'          => $promotion->promotion_name,
-                'start_time'    => $promotion->start_time,
-                'end_time'      => $promotion->end_time,
+                'start_time'    => Carbon::parse($promotion->start_time)->format("d/m/Y"),
+                'end_time'      => Carbon::parse($promotion->end_time)->format("d/m/Y"),
                 'conditions'    => $promotion->details,
                 'status'        => $promotion->status,
                 'conditions'    => json_decode($promotion->conditions),
